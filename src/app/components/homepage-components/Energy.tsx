c669c7b4b350eeec013ae4bfb979267a5ca8e8ad
Neverm1ndEZ <mihirsuman1@gmail.com>
2024-09-21 15:22:38 +0530

    energy-section-added

---
+++ src/app/components/homepage-components/Energy.tsx
@@ -0,0 +1,77 @@
+import { MoveUpRight } from "lucide-react";
+
+export default function Energy() {
+	return (
+		<div className="p-4 lg:p-0">
+			<div className="flex justify-start border-b border-black">
+				<h1 className="text-base uppercase font-semibold text-center">
+					Energy
+				</h1>
+			</div>
+			<div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center lg:items-start py-8 gap-y-6">
+				<div className="flex flex-col items-start lg:w-1/2">
+					<h1 className="text-4xl font-semibold">Clean Power</h1>
+					<p className="text-sm lg:max-w-[350px] mt-4 font-light">
+						Powering the future with clean, renewable solar energy. Join us for
+						a brighter, sustainable tomorrow.
+					</p>
+				</div>
+				<div className="flex flex-col items-start lg:w-1/2 gap-y-6">
+					<div className="font-light">
+						Join <span className="font-semibold">Binny</span> to lead the future
+						of renewable energy with innovative solar solutions. Contribute to
+						sustainability and professional growth
+					</div>
+					<div className="flex items-center gap-x-28 w-3/4">
+						<div className="flex flex-col items-start justify-start gap-y-8">
+							<div>
+								<h1 className="text-3xl font-light border-b border-black pb-2 w-full">
+									85%
+								</h1>
+								<p className="font-light uppercase text-[10px] pt-2">
+									Financial Returns
+								</p>
+							</div>
+							<div>
+								<h1 className="text-3xl font-light border-b border-black pb-2 w-full">
+									75%
+								</h1>
+								<p className="font-light uppercase text-[10px] pt-2">
+									Adoption Rates
+								</p>
+							</div>
+						</div>
+						<div className="flex flex-col items-start justify-start gap-y-8">
+							<div>
+								<h1 className="text-3xl font-light border-b border-black pb-2 w-full">
+									95%
+								</h1>
+								<p className="font-light uppercase text-[10px] pt-2">
+									System Performance
+								</p>
+							</div>
+							<div>
+								<h1 className="text-3xl font-light border-b border-black pb-2 w-full">
+									65%
+								</h1>
+								<p className="font-light uppercase text-[10px] pt-2">
+									Customer Growth
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="flex items-start">
+						<button className="flex flex-row-reverse items-center gap-3 rounded-full px-8 py-4 transition duration-300 ease-in-out group relative">
+							<span className="absolute inset-0 rounded-full border-2 border-black group-hover:border-transparent"></span>
+							<span className="absolute inset-0 rounded-full bg-black opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
+							<MoveUpRight className="w-5 h-5 relative z-10 group-hover:filter group-hover:brightness-0 group-hover:invert transition-all duration-300" />
+							<span className="relative z-10 group-hover:text-white transition-colors duration-300">
+								Get in Touch
+							</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-import Image from "next/image";
+import Hero from "./components/homepage-components/Hero";
+import Energy from "./components/homepage-components/Energy";
 
 export default function Home() {
 	return (
-		<div className="max-w-[1000px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-			<div className="flex flex-col justify-center items-center">
-				<h1 className="text-3xl lg:text-7xl font-bold mb-4 max-w-[350px] lg:max-w-full">
-					Lean energy for{" "}
-					<span className="text-white bg-gradient-to-r from-lime-700 to-blue-300 px-4 py-1">
-						bright
-					</span>{" "}
-					tomorrow.
-				</h1>
-				<p className="text-sm lg:text-base font-light mb-4 lg:mb-8 max-w-[350px] lg:max-w-full">
-					Join us in going solar: Save money, protect Earth, and power your
-					future sustainably.
-				</p>
-				<button className="flex items-center gap-3 rounded-full border border-black px-10 py-4 lg:px-14 lg:py-6 hover:bg-blue-600 hover:text-white hover:border-none transition duration-300 ease-in-out hover:scale-95 group">
-					<Image
-						src="/button-logo.svg"
-						alt="arrow"
-						width={50}
-						height={50}
-						className="group-hover:filter group-hover:brightness-0 group-hover:invert transition-all duration-300"
-					/>
-					<span className="mt-2">Go Solar Now!</span>
-				</button>
-			</div>
+		<div>
+			<Hero />
+			<Energy />
 		</div>
 	);
 }
